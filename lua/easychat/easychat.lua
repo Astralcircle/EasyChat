9bef7ea50bc6d19edece868e1e5584d004fee621
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-08-13 21:42:40 +0300

    Replace image service

--- lua/easychat/easychat.lua
+++ lua/easychat/easychat.lua
@@ -1108,44 +1108,120 @@ if CLIENT then
 			return
 		end
 
-		local decoded_body = util.JSONToTable(body)
-		if not decoded_body then
-			on_imgur_failure("could not json decode body")
-			return
-		end
+		EasyChat.Print(("imgur uploaded: %s"):format(tostring(body)))
+		return body
+	end
 
-		if not decoded_body.success then
-			on_imgur_failure(("%s: %s"):format(
-				decoded_body.status or "unknown status?",
-				decoded_body.data and decoded_body.data.error or "unknown error"
-			))
-			return
-		end
+	local mimeLookup = {
+		jpg = "image/jpeg",
+		jpeg = "image/jpeg",
+		png = "image/png",
+		gif = "image/gif",
+		tif = "image/tiff",
+		tiff = "image/tiff",
+		bmp = "image/bmp",
+		ico = "image/x-icon",
+		txt = "text/plain",
+		html = "text/html",
+		htm = "text/html",
+		css = "text/css",
+		js = "application/javascript",
+		json = "application/json",
+		xml = "application/xml",
+		pdf = "application/pdf",
+		zip = "application/zip",
+		rar = "application/x-rar-compressed",
+		gz = "application/x-gzip",
+		tar = "application/x-tar",
+		mp3 = "audio/mpeg",
+		wav = "audio/wav",
+		ogg = "audio/ogg",
+		mp4 = "video/mp4",
+		webm = "video/webm",
+		mkv = "video/x-matroska",
+		avi = "video/x-msvideo",
+		mov = "video/quicktime",
+		wmv = "video/x-ms-wmv",
+		flv = "video/x-flv",
+		swf = "application/x-shockwave-flash",
+		svg = "image/svg+xml",
+		svgz = "image/svg+xml",
+		eot = "application/vnd.ms-fontobject",
+		ttf = "application/font-sfnt",
+		woff = "application/font-woff",
+		woff2 = "application/font-woff2",
+		otf = "application/font-sfnt",
+	}
 
-		local url = decoded_body.data and decoded_body.data.link
-		if not url then
-			on_imgur_failure("success but link wasn't found?")
-			return
-		end
+	local function getMimeType(filename)
+		local ext = string.GetExtensionFromFilename(filename)
+		return mimeLookup[string.lower(ext)] or "application/octet-stream"
+	end
 
-		EasyChat.Print(("imgur uploaded: %s"):format(tostring(url)))
-		return url
+	local function FormData()
+		return {
+			entries = {},
+			boundary = tostring(math.Round(os.time())),
+
+			Append = function(self, name, value, mime, filename)
+				if mime and not filename then
+					-- Gotta figure out what our params are
+					local isMime = string.find(mime, "/", 1, false) ~= nil
+
+					if not isMime then
+						-- mime is actually the filename
+						filename = mime
+						mime = getMimeType(filename)
+					end
+				end
+
+				if not mime and istable(value) then
+					value = util.TableToJSON(value)
+					mime = "application/json"
+				end
+
+				table.insert(self.entries, {name = name, value = value, mime = mime, filename = filename})
+			end,
+
+			Read = function(self)
+				local body = ""
+
+				for _, entry in ipairs(self.entries) do
+					local mime = entry.mime
+					local name = entry.name
+					local value = entry.value
+					local filename = entry.filename
+
+					body = body .. "--" .. self.boundary
+					body = body .. "\r\nContent-Disposition: form-data; name=\"" .. name .. "\""
+					if filename then
+						body = body .. "; filename=\"" .. filename .. "\""
+					end
+
+					body = body .. "\r\nContent-Type: " .. (mime or "text/plain") .. "; charset=utf-8"
+					body = body .. "\r\n\r\n" .. value .. "\r\n"
+				end
+
+				body = body .. "--" .. self.boundary .. "--\r\n"
+
+				return body
+			end,
+
+			GetHeaders = function(self)
+				return {["Content-Length"] = #self:Read(), ["Content-Type"] = "multipart/form-data; charset=utf-8; boundary=" .. self.boundary}
+			end,
+		}
 	end
 
 	function EasyChat.UploadToImgur(img_base64, callback)
 		local ply_nick, ply_steamid = LocalPlayer():Nick(), LocalPlayer():SteamID()
-		local params = {
-			image = img_base64,
-			type = "base64",
-			name = tostring(os.time()),
-			title = ("%s - %s"):format(ply_nick, ply_steamid),
-			description = ("%s (%s) on %s"):format(ply_nick, ply_steamid, os.date("%d/%m/%Y at %H:%M")),
-		}
 
-		local headers = {}
-		headers["Authorization"] = "Client-ID 1120fa01806a36d"
+		local form = FormData()
+		form:Append("time", "1h")
+		form:Append("reqtype", "fileupload")
+		form:Append("fileToUpload", util.Base64Decode(img_base64), "image.png")
 
-		local http_data = {
+		HTTP({
 			failed = function(...)
 				on_imgur_failure(...)
 				callback(nil)
@@ -1154,14 +1230,13 @@ if CLIENT then
 				local url = on_imgur_success(...)
 				callback(url)
 			end,
-			method = "post",
-			url = "https://api.imgur.com/3/image.json",
-			parameters = params,
-			headers = headers,
-		}
+			method = "POST",
+			url = "https://catbox.moe/user/api.php",
+			body = form:Read(),
+			headers = form:GetHeaders(),
+		})
 
-		HTTP(http_data)
-		EasyChat.Print(("sent picture (%s) to imgur"):format(string.NiceSize(#img_base64)))
+		EasyChat.Print(string.format("sent picture (%s) to imgur", string.NiceSize(#img_base64)))
 	end
 
 	local emote_lookup_tables = {}
